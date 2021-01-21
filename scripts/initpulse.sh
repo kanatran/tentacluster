8e1f188b46dcefb6be68e47288c96929da826435
KentoNishi <kento24gs@outlook.com>
2021-01-20 16:44:47 -0800

    Cleaned up commented out code

--- scripts/initpulse.sh
+++ scripts/initpulse.sh
@@ -8,20 +8,8 @@ test -f hasrun || {
     addgroup $USER audio
     addgroup $3 audio
 
-    # apt install nautilus
-    # nautilus &
-
-    # https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/SystemWide/
-    # systemctl --global disable pulseaudio.service pulseaudio.socket
-
-    # https://superuser.com/questions/1223118/emulating-microphone-input-to-chrome-inside-docker-container
-
-    # export DBUS_SESSION_BUS_ADDRESS=$(cat /proc/$(pidof nautilus | cut -f1 -d" ")/environ | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-)
-
     echo "Load pulseaudio virtual audio source 1"
-    # init pulseaudio server here
-
-    # su -c "sh ./scripts/initpulse.sh $2" $1
+    
     echo "sudo -u $3 sh ./scripts/run-runner.sh $2"
     sudo -u $3 ./scripts/run-runner.sh $2 $4 $5 $6
     echo "Done with running script"
--- scripts/run-runner.sh
+++ scripts/run-runner.sh
@@ -23,17 +23,6 @@ pacmd list-sinks
 echo Sources:
 pacmd list-sources && echo listed sources
 
-# function mplay() {
-#     echo Playing $1
-#     mpv --no-video $1 &> /dev/null
-#     echo Finished playing $1
-#     pkill uvicorn
-#     pkill chrome
-#     exit 0
-# }
-
-# node runner/index.js $1 &
-# cd dictation-kit
 echo Printing release
 youtube-dl --version
 
@@ -46,12 +35,7 @@ git remote set-url origin https://x-access-token:$3@github.com/kanatran/baquap.g
 popd
 popd
 
-# live="https://www.youtube.com/watch?v="$(curl -s https://jetrico.sfo2.digitaloceanspaces.com/hololive/youtube.json | jq .live[$1].yt_video_key | sed 's/"//g')
-
-# live="https://www.youtube.com/watch?v=fxhrQCIeSOQ"
-echo Mashine $1
-# live=$(jq .[$1] links.json | sed 's/"//g')
-# mplay $live &
+echo Machine $1
 
 # Actual python should be at /opt/hostedtoolcache/Python/3.8.7/x64/bin/python3
 echo Using $2
@@ -71,31 +55,3 @@ then
 else
 	echo Channel_ID was null, quiting
 fi
-
-# mpv --no-video $live &
-# echo PCM devices available
-# this works, used for testing
-# arecord -L
-# arecord -r 128000 -d 10 output.wav
-
-# git clone https://github.com/julius-speech/dictation-kit
-# pushd dictation-kit
-# git lfs install
-# # rm julius.dnnconf
-# # cp ../julius.dnnconf julius.dnnconf
-# # padsp 
-# ./bin/linux/julius -C main.jconf -C am-gmm.jconf -input pulseaudio \
-# -fallback1pass \
-# -multipath \
-# -quiet \
-# -nostrip \
-# -rejectshort 0 \
-# -realtime \
-# -forcedict &
-# 
-# sleep 10
-# echo Transcribing $live 
-# mplay $live
-# 
-# # sh run-linux-gmm.sh -input mic -nostrip
-# popd
