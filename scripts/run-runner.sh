2837e24da4b37a8e8eaa42e5eb19788327acae89
KentoNishi <kento24gs@outlook.com>
2021-01-22 21:19:57 -0800

    Clone baquap step

--- scripts/run-runner.sh
+++ scripts/run-runner.sh
@@ -52,15 +52,6 @@ bruhpv &
 echo Printing release
 youtube-dl --version
 
-pushd ..
-git clone https://github.com/Botrans/baquap
-pushd baquap
-git config --global user.name 'Kanatran'
-git config --global user.email '<>'
-git remote set-url origin https://x-access-token:$3@github.com/kanatran/baquap.git
-popd
-popd
-
 echo Machine $1
 
 # Actual python should be at /opt/hostedtoolcache/Python/3.8.7/x64/bin/python3
