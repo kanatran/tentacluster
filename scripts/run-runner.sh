32bf73c3b806c2b11591e7c566ee502b065bbb46
KentoNishi <kento24gs@outlook.com>
2021-01-22 17:21:43 -0800

    Logs for debug

--- scripts/run-runner.sh
+++ scripts/run-runner.sh
@@ -17,12 +17,13 @@ echo "Create a virtual audio source linked up to the virtual microphone output"
 pacmd load-module module-virtual-source source_name="CustomMicrophone"
 
 # Used for debugging audio
-# echo Soundcards:
-# pacmd list soundcards
-# echo Sinks:
-# pacmd list-sinks
-# echo Sources:
-# pacmd list-sources && echo listed sources
+pulseaudio --check -vvvv
+echo Soundcards:
+pacmd list soundcards
+echo Sinks:
+pacmd list-sinks
+echo Sources:
+pacmd list-sources && echo listed sources
 
 
 function bruhpv() {
